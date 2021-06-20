8b8e7b3046f8b7f291696a406d87b7cbc3ad3e05
TUAN\Tuan <tuannguyentn2504@gmail.com>
2021-06-20 16:28:12 +0700

    [component] Add more topics tag

--- frontend/src/components/Contribution/TopicSelect.js
+++ frontend/src/components/Contribution/TopicSelect.js
@@ -2,61 +2,11 @@ import { Button, Collapse } from '@material-ui/core';
 import Grid from '@material-ui/core/Grid';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import animalIcon from 'assets/icons/topics/animal.png';
-import bodyIcon from 'assets/icons/topics/body.png';
-import feelIcon from 'assets/icons/topics/feel.png';
-import foodIcon from 'assets/icons/topics/food.png';
-import healthIcon from 'assets/icons/topics/health.png';
-import jobIcon from 'assets/icons/topics/job.png';
-import socialIcon from 'assets/icons/topics/social.png';
-import treeIcon from 'assets/icons/topics/tree.png';
 import Tag from 'components/UI/Tag';
+import { TOPICS } from 'constant/topics';
 import PropTypes from 'prop-types';
 import React, { useState } from 'react';
 import { topicStyle as useStyle } from './style';
-
-const topics = [
-  {
-    key: '0',
-    title: 'Thực vật',
-    icon: treeIcon,
-  },
-  {
-    key: '1',
-    title: 'Xã hội',
-    icon: socialIcon,
-  },
-  {
-    key: '2',
-    title: 'Nghề nghiệp',
-    icon: jobIcon,
-  },
-  {
-    key: '3',
-    title: 'Đồ ăn',
-    icon: foodIcon,
-  },
-  {
-    key: '4',
-    title: 'Cơ thể con người',
-    icon: bodyIcon,
-  },
-  {
-    key: '5',
-    title: 'Cảm xúc',
-    icon: feelIcon,
-  },
-  {
-    key: '6',
-    title: 'Con vật',
-    icon: animalIcon,
-  },
-  {
-    key: '7',
-    title: 'Sức khoẻ',
-    icon: healthIcon,
-  },
-];
 
 function TopicSelect({ onChange, resetFlag }) {
   const classes = useStyle();
@@ -77,7 +27,7 @@ function TopicSelect({ onChange, resetFlag }) {
       <Grid item xs={12} className={visible ? '' : classes.tagsWrap}>
         <Collapse in={visible}>
           <div className={classes.tags}>
-            {topics.map((topic, index) => (
+            {TOPICS.map((topic, index) => (
               <Tag
                 resetFlag={resetFlag}
                 iconSrc={topic.icon}
---
+++ frontend/src/constant/topics.js
@@ -0,0 +1,129 @@
+import academyIcon from 'assets/icons/topics/academy.png';
+import animalIcon from 'assets/icons/topics/animal.png';
+import bodyIcon from 'assets/icons/topics/body.png';
+import colorIcon from 'assets/icons/topics/color.png';
+import cultureIcon from 'assets/icons/topics/culture.png';
+import entertainmentIcon from 'assets/icons/topics/entertainment.png';
+import excitingIcon from 'assets/icons/topics/exciting.png';
+import feelIcon from 'assets/icons/topics/feel.png';
+import flagIcon from 'assets/icons/topics/flag.png';
+import foodIcon from 'assets/icons/topics/food.png';
+import healthIcon from 'assets/icons/topics/health.png';
+import hobbyIcon from 'assets/icons/topics/hobby.png';
+import jobIcon from 'assets/icons/topics/job.png';
+import othersIcon from 'assets/icons/topics/others.png';
+import skillIcon from 'assets/icons/topics/skill.png';
+import socialIcon from 'assets/icons/topics/social.png';
+import spiritualityIcon from 'assets/icons/topics/spirituality.png';
+import sportIcon from 'assets/icons/topics/sport.png';
+import technologyIcon from 'assets/icons/topics/technology.png';
+import travelIcon from 'assets/icons/topics/travel.png';
+import treeIcon from 'assets/icons/topics/tree.png';
+
+export const TOPICS = [
+  {
+    key: '0',
+    title: 'Thực vật',
+    icon: treeIcon,
+  },
+  {
+    key: '1',
+    title: 'Đời sống',
+    icon: socialIcon,
+  },
+  {
+    key: '2',
+    title: 'Sức khoẻ',
+    icon: healthIcon,
+  },
+  {
+    key: '3',
+    title: 'Ẩm thực',
+    icon: foodIcon,
+  },
+  {
+    key: '4',
+    title: 'Văn hoá',
+    icon: cultureIcon,
+  },
+  {
+    key: '5',
+    title: 'Tính cách',
+    icon: feelIcon,
+  },
+  {
+    key: '6',
+    title: 'Động vật',
+    icon: animalIcon,
+  },
+  {
+    key: '7',
+    title: 'Kỹ năng',
+    icon: skillIcon,
+  },
+  {
+    key: '8',
+    title: 'Học thuật',
+    icon: academyIcon,
+  },
+  {
+    key: '9',
+    title: 'Công nghệ',
+    icon: technologyIcon,
+  },
+  {
+    key: '10',
+    title: 'Con người',
+    icon: bodyIcon,
+  },
+  {
+    key: '11',
+    title: 'Nghề nghiệp',
+    icon: jobIcon,
+  },
+  {
+    key: '12',
+    title: 'Giải trí',
+    icon: entertainmentIcon,
+  },
+  {
+    key: '13',
+    title: 'Sở thích',
+    icon: hobbyIcon,
+  },
+  {
+    key: '14',
+    title: 'Thể thao',
+    icon: sportIcon,
+  },
+  {
+    key: '15',
+    title: 'Du lịch',
+    icon: travelIcon,
+  },
+  {
+    key: '16',
+    title: 'Đất nước',
+    icon: flagIcon,
+  },
+  {
+    key: '17',
+    title: 'Màu sắc',
+    icon: colorIcon,
+  },
+  {
+    key: '18',
+    title: 'Tâm linh',
+    icon: spiritualityIcon,
+  },
+  {
+    key: '19',
+    title: 'Thú vị',
+    icon: excitingIcon,
+  },
+  {
+    key: '20',
+    title: 'Khác',
+    icon: othersIcon,
+  },
+];
