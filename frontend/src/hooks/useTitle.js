08601a324ad0776ff16034f5d8ee85a2392a7e5f
Tuấn Nguyễn <tuannguyentn2504@gmail.com>
2021-07-19 12:25:35 +0700

    ✨ Add meta tag for website

--- frontend/src/hooks/useTitle.js
+++ frontend/src/hooks/useTitle.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from 'react';
 
 // set title for component
-function useTitle(title = 'Dynonary') {
+function useTitle(title = 'Dynonary', isOverride = false) {
   useEffect(() => {
-    document.title = title !== 'Dynonary' ? `${title} - Dynonary` : title;
+    if (isOverride) {
+      document.title = title;
+    } else {
+      document.title = title !== 'Dynonary' ? `${title} - Dynonary` : title;
+    }
   }, []);
 
   return null;
