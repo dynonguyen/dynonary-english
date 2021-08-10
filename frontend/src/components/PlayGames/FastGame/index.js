898b839c6e83e39c5fa97a684b9c8c93d0c63d3a
Tuấn Nguyễn <tuannguyentn2504@gmail.com>
2021-08-10 17:10:27 +0700

    ➕ Add main game component view for fast game

--- frontend/src/components/PlayGames/FastGame/index.js
+++ frontend/src/components/PlayGames/FastGame/index.js
@@ -1,9 +1,34 @@
 import { TOPICS } from 'constant/topics';
-import React from 'react';
+import React, { useState } from 'react';
+import MainFastGame from './main-game';
 import useStyle from './style';
 
 function FastGame() {
   const classes = useStyle();
+  const [topicKey, setTopicKey] = useState(-1);
+
+  const renderTopic = () => (
+    <>
+      <div className={classes.title}>
+        <h1>Chọn một chủ đề</h1>
+      </div>
+      <div className={classes.topics}>
+        {TOPICS.map((topic, index) => (
+          <div
+            key={index}
+            className={classes.topicItem}
+            onClick={() => setTopicKey(topic.key)}>
+            <img
+              src={topic.icon}
+              className={classes.topicImg}
+              alt={topic.title}
+            />
+            <h3 className={classes.topicTitle}>{topic.title}</h3>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 
   return (
     <div className={`flex-center ${classes.wrapper}`}>
@@ -11,21 +36,7 @@ function FastGame() {
 
       <div className="container">
         <div className={classes.root}>
-          <div className={classes.selectTopic}>
-            <h1>Chọn một chủ đề</h1>
-          </div>
-          <div className={classes.topics}>
-            {TOPICS.map((topic, index) => (
-              <div key={index} className={classes.topicItem}>
-                <img
-                  src={topic.icon}
-                  className={classes.topicImg}
-                  alt={topic.title}
-                />
-                <h3 className={classes.topicTitle}>{topic.title}</h3>
-              </div>
-            ))}
-          </div>
+          {topicKey < 0 ? renderTopic() : <MainFastGame topic={topicKey} />}
         </div>
       </div>
     </div>
--- frontend/src/components/PlayGames/FastGame/style.js
+++ frontend/src/components/PlayGames/FastGame/style.js
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
   wrapper: {
     width: '100vw',
     minHeight: '100vh',
@@ -25,20 +25,22 @@ export default makeStyles(() => ({
     backgroundColor: 'var(--bg-color-sec)',
     borderRadius: '16px',
     border: 'solid 10px rgba(127,127,127,0.5)',
+    overflow: 'hidden',
   },
 
-  selectTopic: {
-    padding: '1.2rem 2.4rem',
+  title: {
+    padding: '1.6rem',
     textAlign: 'center',
 
     '& h1': {
       border: 'solid 5px rgba(127,127,127,0.5)',
       width: 'max-content',
-      padding: '1.6rem',
+      padding: '2.4rem',
       margin: 'auto',
       fontWeight: 500,
       borderRadius: '16px',
       color: 'var(--title-color)',
+      fontSize: '2.4rem',
     },
   },
 
@@ -82,5 +84,77 @@ export default makeStyles(() => ({
   topicTitle: {
     fontSize: '1.6rem',
     fontWeight: 400,
+  },
+
+  answerList: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: '3.2rem',
+  },
+
+  answerItem: {
+    borderRadius: '12px',
+    marginRight: '1.6rem',
+    overflow: 'hidden',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: '1.2rem',
+    width: 80,
+    height: 80,
+    cursor: 'pointer',
+
+    '& img': {
+      width: '100%',
+      height: '100%',
+      transition: 'all .25s',
+
+      '&:hover, &:active': {
+        transform: 'scale(1.2)',
+      },
+    },
+
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      height: 120,
+    },
+
+    [theme.breakpoints.up('md')]: {
+      width: 180,
+      height: 180,
+    },
+  },
+
+  timerWrap: {
+    width: '80%',
+    flexBasis: '64px',
+    textAlign: 'center',
+    border: 'solid 5px var(--grey)',
+    margin: '0 auto 1.2rem',
+    borderRadius: '12px',
+    overflow: 'hidden',
+    position: 'relative',
+  },
+
+  timer: {
+    width: '100%',
+    height: '100%',
+    transition: 'width 0.5s',
+    backgroundColor: 'var(--primary-color)',
+    background:
+      'linear-gradient(90deg, var(--primary-color) 0%, var(--secondary-color) 100%)',
+  },
+
+  timeStr: {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%,-50%)',
+    fontSize: '1.6rem',
+    color: '#fff',
+    fontWeight: 500,
+    letterSpacing: '1px',
   },
 }));
